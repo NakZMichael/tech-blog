e4433a79179e4ff2a294d2ee3f13819b8427b475
NakZMichael <nakazato.muka@gmail.com>
2021-04-20 07:35:17 +0900

    fix: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ import {BaseLayout} from "../components/BaseLayout";
 import { PostList } from "../components/PostsList";
 import SEO from "../components/seo";
 import { Introduction } from "../components/Introduction";
+import { Header1 } from "../components/BlogElements";
+import { Typography } from "@material-ui/core";
 export default function Home({data}) {
   return (
     <BaseLayout>
       <SEO title={data.site.siteMetadata.title} description={data.site.siteMetadata.description} />
       <Introduction></Introduction>
+      <Typography variant="h3" component="h2" style={{textAlign:"center"}}>記事一覧</Typography>
       <PostList nodes={data.allMdx.nodes as any[]} />
     </BaseLayout>
   );
